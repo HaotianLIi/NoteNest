6ef075cfb6f33a02562828e2424188ea0ce90266
HaotianLi <frankyhaotian@gmail.com>
2024-11-12 14:28:43 -0500

    Add getNotesByIdController in controller to retrieve note data by ID and send back to client.

--- Backend/controller/notesController.js
+++ Backend/controller/notesController.js
@@ -1,19 +1,33 @@
-const {getNotesTable} = require('../model/notesModel')
+const {getNotesTable,getNotesById} = require('../model/notesModel')
 
-const getAllNotes = async (req, res) => {
+const getAllNotesController = async (req, res) => {
     try {
         const result = await getNotesTable()
         res.json(result)
     } catch (err) {
-        console.error('Error in getAllNotes: ', err.message)
+        console.error('Error in getAllNotesController: ', err.message)
         if (err.message === 'Data Not Found'){
             res.status(404).send('No notes data found')
         }
         res.status(500).send(`Error message: ${err.message}`)
     }
+}
 
+const getNotesByIdController = async (req, res) => {
+    const id = req.params.id
+    try {
+        const result = await getNotesById(id)
+        res.json(result)
+    } catch (err){
+        console.error('Error in getNotesByIdController: ', err.message)
+        if (err.message === 'id not found'){
+            res.status(404).send('Notes id not found')
+        }
+        res.status(500).send(`Error message: ${err.message}`)
+    }
 }
 
 module.exports = {
-    getAllNotes
+    getAllNotesController,
+    getNotesByIdController
 }
