496c4c1792510ad5a019095834f07a6581d75f90
HaotianLi <frankyhaotian@gmail.com>
2024-11-12 14:23:12 -0500

    Add controller function getStatusByIdController to handle fetching status by ID and respond to client requests.

--- Backend/controller/statusController.js
+++ Backend/controller/statusController.js
@@ -1,12 +1,12 @@
-const { getStatusTable } = require('../model/statusModel')
+const { getStatusTable, getStatusById } = require('../model/statusModel')
 
 
-const getAllStatus = async (req, res) => {
+const getAllStatusController = async (req, res) => {
     try {
         const result = await getStatusTable()
         res.json(result)
     } catch (err){
-        console.error('Error in getAllStatus: ', err.message)
+        console.error('Error Catched in getAllStatusController: ', err.message)
         if(err.message === 'No data Found'){
             res.status(404).send('No status data found')
         } 
@@ -14,9 +14,23 @@ const getAllStatus = async (req, res) => {
     }
 }
 
+const getStatusByIdController = async (req, res) => {
+    const id = req.params.id
+    try {
+        const result = await getStatusById(id)
+        res.json(result)
+    } catch (err) {
+        console.log('Error Catched in getStatusByIdController: ', err)
+        if (err.message === 'Status id not found'){
+            res.status(404).send('Status id not found')
+        }
+        res.status(500).send(`Error message: ${err.message}`)
+    }
+}
 
 module.exports = {
-    getAllStatus
+    getAllStatusController,
+    getStatusByIdController
 }
 
 
