3e86c5c1c3849d3233207c921336f16faa025d6d
HaotianLi <frankyhaotian@gmail.com>
2024-11-12 10:04:05 -0500

    Refactor error handling in statusConroller

--- Backend/controller/statusController.js
+++ Backend/controller/statusController.js
@@ -1,21 +1,18 @@
 const express = require('express')
 const app = express()
-const { getStatus } = require('../model/statusModel')
+const { getStatusTable } = require('../model/statusModel')
 
 
 const getAllStatus = async (req, res) => {
     try {
-        const result = await getStatus()
+        const result = await getStatusTable()
         res.json(result)
     } catch (err){
         console.error('Error in getAllStatus: ', err.message)
         if(err.message === 'No data Found'){
             res.status(404).send('No status data found')
-        } else if (err.message === 'Database error'){
-            res.status(500).send('Database error occured')
-        } else {
-            res.status(500).send('Unexpected error occurred')
-        }
+        } 
+        res.status(500).send(`Error message:  ${err.message}`)
     }
 }
 
