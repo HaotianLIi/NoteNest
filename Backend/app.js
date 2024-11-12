a463ee5d8e2d5e093119f45318f561cebba4c7ad
HaotianLi <frankyhaotian@gmail.com>
2024-11-12 10:20:19 -0500

    Add /notes endpoint to retrieve all notes data

--- Backend/app.js
+++ Backend/app.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const app = express()
 const { getAllStatus } = require('./controller/statusController')
+const { getAllNotes } = require('./controller/notesController')
 
 app.get('/', (req,res) =>{
     res.send('Sup')
 })
 
 app.get('/status', getAllStatus)
-
+app.get('/notes', getAllNotes)
 
 
 
