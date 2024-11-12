4c588e25159f6d44ee2a6f0e91ef0391ddd266d7
HaotianLi <frankyhaotian@gmail.com>
2024-11-12 14:19:36 -0500

    Add function getStatusById in model to fetch status by ID from the database with error handling.

--- Backend/model/statusModel.js
+++ Backend/model/statusModel.js
@@ -14,6 +14,20 @@ const getStatusTable = async() => {
     }
 }
 
+const getStatusById = async (id) => {
+    try {
+        const result = await db.one('SELECT * FROM status WHERE id = $1', [id])
+        if (result.length === 0){
+            throw new Error('Status id not found')
+        }
+        return result
+    } catch (err) {
+        console.log('Error catched in getStatusById under statusModel: ', err)
+        throw err
+    }
+}
+
 module.exports = {
-    getStatusTable
+    getStatusTable,
+    getStatusById
 }
