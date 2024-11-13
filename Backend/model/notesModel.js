b61e1a910a3a2357c170264fc381df4da7c11d76
HaotianLi <frankyhaotian@gmail.com>
2024-11-13 13:49:21 -0500

    Add create,update,delete functions in notes model for database interactions

--- Backend/model/notesModel.js
+++ Backend/model/notesModel.js
@@ -26,7 +26,56 @@ const getNotesById = async (id) => {
     }
 }
 
+const createNotes = async (title,body,status_id) => {
+    try {
+        const result = await db.one(
+            'INSERT INTO notes (title,body,status_id) VALUES ($1,$2,$3) RETURNING *'
+            ,[title,body,status_id]
+        )
+        console.log("Insert result: ", result)
+        return result
+    } catch (err) {
+        if (err.received === 0){
+            throw new Error('Data not found')
+        }
+        throw err
+    }
+}
+
+const updateNotes = async (title,body,status_id,id) => {
+    try {
+        const result = await db.any(
+            'UPDATE notes SET title = $1, body = $2, status_id = $3 WHERE id = $4 RETURNING *'
+            ,[title,body,status_id,id] 
+        )
+        console.log("Data updated: ", result)
+        return result
+    } catch (err) {
+        if (err.received === 0){
+            throw new Error('Data not found')
+        }
+        throw err
+    }
+}
+
+const deleteNotes = async (id) => {
+    try {
+        const result = await db.result('DELETE FROM notes WHERE id = $1 RETURNING *',[id])
+        console.log(`Rows deleted: ${result.rowCount}`);
+        return result
+    } catch (err) {
+        if(err.received === 0){
+            throw new Error('Data not found')
+        }
+        throw err
+    }
+} 
+
 module.exports = {
     getNotesTable,
-    getNotesById
+    getNotesById,
+    createNotes,
+    updateNotes,
+    deleteNotes
+
 }
