e2abfd5b9eb02b53a4da12c4787a3755804b7a26
HaotianLi <frankyhaotian@gmail.com>
2024-11-13 22:27:31 -0500

    Add error handler for database connection issues in pg-promise configuration

--- Backend/db.js
+++ Backend/db.js
@@ -1,6 +1,13 @@
 require('dotenv').config()
 
-const pgp = require('pg-promise')()
+const pgp = require('pg-promise')({
+    error: (err, e) => {
+        console.log('Error Occrued: ', err.message || err)
+        if(e.cn){
+            console.error('Connection Error: ', e.cn)
+        }
+    }
+})
 const db = pgp(`postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`)
 
 module.exports = db
