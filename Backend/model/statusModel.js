d9b792f9bf975ab9dfddaa9ea968a45f54e34693
HaotianLi <frankyhaotian@gmail.com>
2024-11-14 23:08:26 -0500

    add create,update,delete handler for database interactions in statusModel

--- Backend/model/statusModel.js
+++ Backend/model/statusModel.js
@@ -27,7 +27,52 @@ const getStatusById = async (id) => {
     }
 }
 
+const createStatus = async (status) => {
+    try {
+        const result = await db.oneOrNone('INSERT INTO status (status) VALUES ($1) RETURNING *', [status])
+        console.log('status added: ', result)
+        return result
+    } catch (err) {
+        if (err.recevied === 0){
+            throw new Error('add new status falied')
+        }
+        throw err
+    }
+}
+
+const deleteStatus = async (id) => {
+    try {
+        const result = await db.oneOrNone('DELETE FROM status WHERE id = $1 RETURNING *',[id])
+        if (!result){
+            throw new Error('status none exist')
+        }
+        return result
+    } catch (err) {
+        console.error('Error deleting status: ', err)
+        throw err
+    }
+}
+
+const updateStatus = async (status,id) => {
+    try {
+        const result = await db.oneOrNone('UPDATE status SET status = $1 WHERE id = $2 RETURNING *',
+            [status,id]
+        )
+        if(!result){
+            throw new Error('Update id not found')  
+        } 
+        console.log('Update successfully.', result)
+        return result
+    } catch (err) {
+        console.log('Error catched in updateStatus: ', err)
+        throw err
+    }
+}
+
 module.exports = {
     getStatusTable,
-    getStatusById
+    getStatusById,
+    createStatus,
+    deleteStatus,
+    updateStatus
 }
