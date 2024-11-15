137719853a69e5ab57fd8b3fbe399765a28f7792
HaotianLi <frankyhaotian@gmail.com>
2024-11-14 23:15:08 -0500

    update create,update,delete routes for status in app.js

--- Backend/app.js
+++ Backend/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 app.use(express.json()) // now express can parse json for incoming request like req.body
 
-const { getAllStatusController, getStatusByIdController } = require('./controller/statusController')
+const { getAllStatusController, getStatusByIdController,createStatusController,deleteStatusController, updateStatusController } = require('./controller/statusController')
 const { getAllNotesController, getNotesByIdController, createNotesController,updateNotesController,deleteNotesController } = require('./controller/notesController')
 
 app.get('/', (req, res) => {
@@ -12,16 +12,24 @@ app.get('/', (req, res) => {
 
 app.get('/notes', getAllNotesController)
 app.get('/notes/:id',getNotesByIdController)
+
+
+
+app.post('/notes/create',createNotesController)
+app.put('/notes/update/:id', updateNotesController)
+app.delete('/notes/delete/:id',deleteNotesController)
+
 app.get('/status', getAllStatusController)
+app.post('/status/create', createStatusController)
+app.put('/status/update/', updateStatusController)
+app.delete('/status/delete/:id', deleteStatusController)
+
 app.get('/status/:id',getStatusByIdController)
 
-app.post('notes/create',createNotesController)
-app.put('notes/update/:id', updateNotesController)
-app.delete('notes/delete/:id',deleteNotesController)
-
-
-
-
+// app.get('/test', (req, res) => {
+//     console.log('Test route working')
+//     res.send("Test route working");
+// });
 
 app.listen(3000, ()=> {
     console.log('Server is running on http://localhost:3000')
