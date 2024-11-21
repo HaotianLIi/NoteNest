1703a24396196c17462012ca5fbe16a4fa5c3f4c
HaotianLi <frankyhaotian@gmail.com>
2024-11-21 18:26:09 -0500

    emove error handling, interaction with data now

--- Backend/model/notesModel.js
+++ Backend/model/notesModel.js
@@ -2,41 +2,36 @@ const db = require('../db')
 
 const getNotesTable = async () => {
     const result = db.any('SELECT * FROM notes')
-    if(!result) throw new Error('Data Not Found')
     console.log("Get notes table succeed: ", result)
     return result
 }
 
 const getNotesById = async (id) => {
     const result = await db.one('SELECT * FROM notes WHERE id = $1', [id])
-    if (!result) throw new Error('id not found')
     console.log("Get notes id succeed: ", result)
     return result
 }
 
-const createNotes = async (title,body,status_id) => {
+const createNotes = async (title,content,status_id) => {
     const result = await db.one(
-        'INSERT INTO notes (title,body,status_id) VALUES ($1,$2,$3) RETURNING *'
-        ,[title,body,status_id]
+        'INSERT INTO notes (title,content,status_id) VALUES ($1,$2,$3) RETURNING *'
+        ,[title,content,status_id]
     )
-    if (!result) throw new Error('Creation Failed')
     console.log("Created Succeed: ", result)
     return result
 }
 
-const updateNotes = async (title,body,status_id,id) => {
+const updateNotes = async (title,content,status_id,id) => {
     const result = await db.any(
-        'UPDATE notes SET title = $1, body = $2, status_id = $3 WHERE id = $4 RETURNING *'
-        ,[title,body,status_id,id] 
+        'UPDATE notes SET title = $1, content = $2, status_id = $3 WHERE id = $4 RETURNING *'
+        ,[title,content,status_id,id] 
     )
-    if(!result) throw new Error('Update Failed')
     console.log("Updated succeed: ", result)
     return result
 }
 
 const deleteNotes = async (id) => {
     const result = await db.result('DELETE FROM notes WHERE id = $1 RETURNING *',[id])
-    if (!result) throw new Error('Notes none exist')
     console.log('Delete succeed: ', result)
     return result
 }
--- Backend/model/statusModel.js
+++ Backend/model/statusModel.js
@@ -2,28 +2,24 @@ const db = require('../db')
 
 const getStatusTable = async() => {
     const result = await db.any('SELECT * FROM status')
-    if(!result) throw new Error('No data Found')
     console.log('Data Found: ', result)
     return result
 }
 
 const getStatusById = async (id) => {
     const result = await db.one('SELECT * FROM status WHERE id = $1', [id])
-    if (!result) throw new Error('Status id not found')
     console.log('Data id Found: ', result)
     return result
 }
 
 const createStatus = async (status) => {
     const result = await db.oneOrNone('INSERT INTO status (status) VALUES ($1) RETURNING *', [status])
-    if(!result) throw new Error('Add new status failed')
     console.log(`status '${result.status}' added in table`)
     return result
 }
 
 const deleteStatus = async (id) => {
     const result = await db.oneOrNone('DELETE FROM status WHERE id = $1 RETURNING *',[id])  
-    if(!result) throw new Error('Delete status failed')
     console.log('status model ', result)
     return result
 }
@@ -32,7 +28,6 @@ const updateStatus = async (status,id) => {
     const result = await db.oneOrNone('UPDATE status SET status = $1 WHERE id = $2 RETURNING *',
         [status,id]
     )
-    if(!result) throw new Error('Update status failed')  
     console.log('Update successfully.', result)
     return result
 }
