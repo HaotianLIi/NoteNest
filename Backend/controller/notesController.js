5b0aa142318e29545f2e66291911cf0b18b4aef1
HaotianLi <frankyhaotian@gmail.com>
2024-11-22 08:08:23 -0500

    update specific error handling in notesController

--- Backend/controller/notesController.js
+++ Backend/controller/notesController.js
@@ -3,82 +3,87 @@ const {getNotesTable,getNotesById,createNotes,updateNotes,deleteNotes} = require
 const getAllNotesController = async (req, res) => {
     try {
         const result = await getNotesTable()
+        if(!result){
+            return res.status(404).send({message: 'Error message: Notes data none exist in notes table'})
+        }
         res.status(200).json({message: 'Displayed Row', data: result})
     } catch (err) {
-        if (err.message === 'Data Not Found'){
-            res.status(404).send('Error message: Notes data none exist in notes table')
-        }
         res.status(500).send({message: `Error Occured: ${err.message}`})
     }
 }
 
 const getNotesByIdController = async (req, res) => {
     const id = req.params.id
-
-    if(!id) return res.status(400).send({message: 'Validation: Missing required field'})
+    if(!id){
+        return res.status(400).send({message: 'Validation: Missing id required field'})
+    } 
 
     try {
         const result = await getNotesById(id)
+        if(!result){
+            return res.status(404).send('Error message: notes id not found in notes table')
+        }
         res.status(200).json({message: 'Dispalyed Row', data: result})
     } catch (err){
-        if (err.message === 'id not found'){
-            res.status(404).send('Error message: notes id not found in notes table')
-        }
         res.status(500).send({message: `Error Occured: ${err.message}`})
     }
 }
 
 const createNotesController = async (req, res) => {
-    const {title,body,status_id} = req.body
+    const {title, content, status_id} = req.body
 
-    if(!title || !status_id) return res.status(400).send({message: 'Validation: Missing required field'})
+    const REQUIRED_FIELD = ['title','content','status_id']
+    const MissingField = REQUIRED_FIELD.filter(column => !req.body[column])
+    if(MissingField.length > 0){
+        return res.status(400).send({message: 'Validation: Missing required field', Missing: MissingField})
+    }
 
     try {
-        const result = await createNotes(title,body,status_id)
-        res.status(200).json({message: 'Created Row', data: result})
-    } catch (err) {
-        if (err.message === 'Creation Failed'){
-            res.status(404).send('Error message: notes creation failed in notes table')
-        } else {
-            res.status(500).send({message: `Error Occured: ${err.message}`})
+        const result = await createNotes(title,content,status_id)
+        if(!result){
+            return res.status(404).send('Error message: notes creation failed in notes table')
         }
+        res.status(201).json({message: 'Created Row', data: result})
+    } catch (err) {
+        res.status(500).send({message: `Error Occured: ${err.message}`})
         
     }
 }
 
 const updateNotesController = async (req, res) => {
-    const {title, body, status_id, id } = req.body
+    const {title, content, status_id, id} = req.body
 
-    if(!title || !status_id || !id) return res.status(400).send({message: 'Validation: Missing required field'})
+    const REQUIRED_FIELD = ['title','content','status_id','id']
+    const MissingField = REQUIRED_FIELD.filter(column => !req.body[column])
+    if(MissingField.length > 0){
+        return res.status(400).send({message: 'Validation: Missing required filed', missing: MissingField})
+    }
 
     try {
-        const result = await updateNotes(title,body,status_id,id)
+        const result = await updateNotes(title,content,status_id,id)
+        if(!result){
+            return res.status(404).send('Error message: update notes rows failed in notes table')
+        }
         res.status(200).json({message: 'Updated Row', data: result})
     } catch (err) {
-        console.log('Error catched in updateNotesController: ', err)
-        if (err.message === 'Update Failed'){
-            res.status(404).send('Error message: update notes rows failed in notes table')
-        } else {
-            res.status(500).send({message: `Error Occured: ${err.message}`})
-        }
-        
+        res.status(500).send({message: `Error Occured: ${err.message}`})    
     }
 }
 
 const deleteNotesController = async (req,res) => {
     const id = req.params.id
-
-    if(!id) return res.status(400).send({message: 'Validation: Missing required field'})
+    if(!id) {
+        return res.status(400).send({message: 'Validation: Missing id required field'})
+    }
 
     try {
          const result = await deleteNotes(id)
-         res.status(200).json({message: 'Deleted Row', data: result})
+         if(!result){
+            return res.status(404).send('Error message: Delete target none exist, deletion failed.')
+         }
+         res.status(200).json({message: 'Deleted Row', data: result.rows[0]})
     } catch (err){
-        if (err.message === 'Notes none exist'){
-            res.status(404).send('Error message: Delete target none exist, deletion failed.')
-        } else {
-            res.status(500).send({message: `Error Occured: ${err.message}`})
-        }
+        res.status(500).send({message: `Error Occured: ${err.message}`})
     }
 }
 
