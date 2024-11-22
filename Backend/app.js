f5867d2ca89dfca9a348b5d6894a71dd06a723bb
HaotianLi <frankyhaotian@gmail.com>
2024-11-22 08:15:33 -0500

    modify the table structure and update routes status in app.js

--- Backend/app.js
+++ Backend/app.js
@@ -4,33 +4,35 @@ app.use(express.json()) // now express can parse json for incoming request like 
 
 const { getAllStatusController, getStatusByIdController,createStatusController,deleteStatusController, updateStatusController } = require('./controller/statusController')
 const { getAllNotesController, getNotesByIdController, createNotesController,updateNotesController,deleteNotesController } = require('./controller/notesController')
+const { getNotesStatusController, getNoteStatusByIdController} = require('./controller/notesStatusController')
 
 app.get('/', (req, res) => {
     res.send('sup')
 });
 
-
+// Get notes table info
 app.get('/notes', getAllNotesController)
 app.get('/notes/:id',getNotesByIdController)
 
-
-
+//Create update delete notes
 app.post('/notes/create',createNotesController)
 app.put('/notes/update/:id', updateNotesController)
 app.delete('/notes/delete/:id',deleteNotesController)
 
+//Get status table info
 app.get('/status', getAllStatusController)
+app.get('/status/:id',getStatusByIdController)
+
+//Create update delete status
 app.post('/status/create', createStatusController)
 app.put('/status/update/', updateStatusController)
 app.delete('/status/delete/:id', deleteStatusController)
 
-app.get('/status/:id',getStatusByIdController)
+//Get notes_status table info
+app.get('/notes_status', getNotesStatusController)
+app.get('/notes_status/:id', getNoteStatusByIdController)
 
-// app.get('/test', (req, res) => {
-//     console.log('Test route working')
-//     res.send("Test route working");
-// });
 
-app.listen(3000, ()=> {
-    console.log('Server is running on http://localhost:3000')
+app.listen(5000, ()=> {
+    console.log('Server is running on http://localhost:5000')
 })
